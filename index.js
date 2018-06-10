f8a47ab0d61cdc5121f0de237f5d9b6090819484
Vadim Nicolaev <vadim.vicolaev.v@gmail.com>
2018-06-10 14:49:55 +0300

    add validation for link

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 import TelegramBot from 'node-telegram-bot-api';
 import { TG_TOKEN } from './config/config';
+import { regExp } from './helpers/index';
 
-
-
-//init bot
 const bot = new TelegramBot(TG_TOKEN, { polling: true });
 
 bot.onText(/\/link (.+)/, (msg, match) => {
-    // 'msg' is the received Message from Telegram
-    // 'match' is the result of executing the regexp above on the text content
-    // of the message
+  const { linkRegExp } = regExp;
+  const chatId = msg.chat.id;
+  const link = match[1];
   
-    const chatId = msg.chat.id;
-    const resp = match[1]; // the captured "whatever"
+  if (!linkRegExp.test(link)) {
+    bot.sendMessage(chatId, 'Invalid link');
+    return;  
+  }
   
-    // send back the matched "whatever" to the chat
-    bot.sendMessage(chatId, resp);
-  });
+  bot.sendMessage(chatId, link);
+});
