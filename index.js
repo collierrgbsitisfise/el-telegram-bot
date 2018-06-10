dd56b3eebd2dbd10a3ed8b5f583141716a767e87
Vadim Nicolaev <vadim.vicolaev.v@gmail.com>
2018-06-10 14:31:06 +0300

    echo  after link comand implement

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 import TelegramBot from 'node-telegram-bot-api';
 import { TG_TOKEN } from './config/config';
 
-console.log('I am work');
-console.log(TG_TOKEN);
+
+
+//init bot
+const bot = new TelegramBot(TG_TOKEN, { polling: true });
+
+bot.onText(/\/link (.+)/, (msg, match) => {
+    // 'msg' is the received Message from Telegram
+    // 'match' is the result of executing the regexp above on the text content
+    // of the message
+  
+    const chatId = msg.chat.id;
+    const resp = match[1]; // the captured "whatever"
+  
+    // send back the matched "whatever" to the chat
+    bot.sendMessage(chatId, resp);
+  });
