802ab4c7d96a9283eaf4f8e1573863f749e57463
Vadim Nicolaev <vadim.vicolaev.v@gmail.com>
2018-06-24 16:16:23 +0300

    change regExp helper for link validation

--- config/config.js
+++ config/config.js
@@ -7,7 +7,7 @@ const devSettings = {
 
 const prodSettings = {
     "TG_TOKEN": "615902998:AAHU0IZtPTq2HjxXTX1sfkG3mjwQ7s4icCg",
-    "EL_API_URL": "http://209.97.137.33",
+    "EL_API_URL": "http://elshare.co",
     "EL_API_PORT": "5000",
     "API_PREFIX": "/api/v1/es-link"
 }
