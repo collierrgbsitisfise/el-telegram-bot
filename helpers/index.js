37123a74d39b4aac073644fd01071b5aa397910d
Vadim Nicolaev <vadim.vicolaev.v@gmail.com>
2018-07-01 14:40:59 +0300

    add webservice info

--- helpers/index.js
+++ helpers/index.js
@@ -14,12 +14,12 @@ module.exports = {
     },
     cmd: {
         help: {
-            ru: `1) /link "ссылка" : создать короткую ссылку (🇷🇺)&#10;2)/once_available_link "ссылка" : создать короткую и единожды доступную ссылку (🇷🇺)&#10;3)/private_link "ссылка" : создать короткую и привтную(доступную только в приватном[инкогнито] режиме браузера) ссылку (🇷🇺)`,
-            en: `1) /link "linkname" : create short link (🇺🇸)&#10;2)/once_available_link "linkname" : create short and once available link (🇺🇸)&#10;3)/private_link "linkname" : create short and private(wich available from private[incognito] browser mode) link (🇺🇸)`
+            ru: `1) /link "ссылка" : создать короткую ссылку (🇷🇺)&#10;2)/once_available_link "ссылка" : создать короткую и единожды доступную ссылку (🇷🇺)&#10;3)/private_link "ссылка" : создать короткую и привтную(доступную только в приватном[инкогнито] режиме браузера) ссылку (🇷🇺) &#10;Веб сервис - https://elshare.co`,
+            en: `1) /link "linkname" : create short link (🇺🇸)&#10;2)/once_available_link "linkname" : create short and once available link (🇺🇸)&#10;3)/private_link "linkname" : create short and private(wich available from private[incognito] browser mode) link (🇺🇸) &#10;Web service - https://elshare.co`
         },
         start: {
-            ru: `🇷🇺 Вас приветствуйте <b>ELSHARE BOT</b>.C моей помощью вы можете легко создовать <b>короткие</b> ссылик,<b>приватные</b> ссылки (ссылки которые можно открыть в приватном(инкогнито) режиме),ссылки , которые будут доступны <b>единожды</b>`,
-            en: `🇺🇸 You are welcome <b> ELSHARE BOT </b> .With my help, you can easily create <b> short </b> link, <b> private </b> links (links that can be opened from private (incognito) mode), links that will be available <b> once </b>`
+            ru: `🇷🇺 Вас приветствуйте <b>ELSHARE BOT</b>.C моей помощью вы можете легко создовать <b>короткие</b> ссылик,<b>приватные</b> ссылки (ссылки которые можно открыть в приватном(инкогнито) режиме),ссылки , которые будут доступны <b>единожды</b>.Все команды - /help.Веб сервис - https://elshare.co`,
+            en: `🇺🇸 You are welcome <b> ELSHARE BOT </b> .With my help, you can easily create <b> short </b> link, <b> private </b> links (links that can be opened from private (incognito) mode), links that will be available <b> once </b>.All commands - /help. Web service - https://elshare.co`
         }   
     }
 }
